4780b37da2410f40e3f778eecb9e7ee0ecdbf92d
Thibault Dassonvillé <d.tibo95@gmail.com>
2016-07-22 17:33:11 +0200

    menu + partial views

--- js/app.js
+++ js/app.js
@@ -1,13 +1,88 @@
 (function(){
 	
-	var app = angular.module("LoginApp", []);
+	var app = angular.module("App", []);
 		
 	app.controller("controller", ["$scope",function($scope){
 		$scope.username = "";
 		$scope.password = "";
 		$scope.functionLog = function(){
 		  console.log($scope.username, $scope.password);
+		  if($scope.username == "Thibault" && $scope.password == "Dasson_95"){
+			redirect();
+		  } else {
+			alert("Connexion échouée");
+		  }
 		}
 	}]);
 	
+	app.controller("menuController", ["$scope", function($scope){
+		$scope.medecins = function(){
+			// console.log("Ici les médecins");
+			loadPartial("medecins");
+		}
+		$scope.hopitaux = function(){
+			//console.log("Ici les hôpitaux");
+			loadPartial("hopitaux");
+		}
+		$scope.contrats = function(){
+			//console.log("Ici les contrats");
+			loadPartial("contrats");
+		}
+		$scope.planning = function(){
+			//console.log("Ici le planning");
+			loadPartial("planning");
+		}
+	}]);
+	
+	function redirect(){
+		//console.log("Vous allez être redirigé");
+		window.location = "home.html";
+	}
+	
+	function loadPartial(page) {
+        switch (page) {
+            case 'medecins':
+				console.log("medecins");
+                $.ajax({
+                    url: 'medecins.html',
+                    type: 'GET',
+                    dataType: 'html',
+                    success: function (result) {
+                        $('#partial').html(result);
+                    }
+                });
+                break;
+            case 'hopitaux':
+                $.ajax({
+                    url: 'hopitaux.html',
+                    type: 'GET',
+                    dataType: 'html',
+                    success: function (result) {
+                        $('#partial').html(result);
+                    }
+                });
+                break;
+            case 'contrats':
+                $.ajax({
+                    url: 'contrats.html',
+                    type: 'GET',
+                    dataType: 'html',
+                    success: function (result) {
+                        $('#partial').html(result);
+                    }
+                });
+                break;
+            case 'planning':
+                $.ajax({
+                    url: 'planning.html',
+                    type: 'GET',
+                    dataType: 'html',
+                    success: function (result) {
+                        $('#partial').html(result);
+                    }
+                });
+                break;
+            
+        }
+    }
 })();
