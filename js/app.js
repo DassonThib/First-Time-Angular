620ef9e0533fa4acf8e8fb1d65af5e30b1330b82
Thibault Dassonvillé <d.tibo95@gmail.com>
2016-07-27 11:56:06 +0200

    routing fix

--- js/app.js
+++ js/app.js
@@ -7,13 +7,14 @@
 	})	
 	.value('$routerRootComponent', 'app')
 	.component('app', {
-		template: 
+		template:
 			"<nav>\n" +
-			"  <a href='medecins.html'>Médecins</a>\n" +
-			"  <a href='hopitaux.html'>Hôpitaux</a>\n" +
-			"  <a href='contrats.html'>Contrats</a>\n" +
-			"  <a href='planning.html'>Planning</a>\n" +
+			"  <a href='medecins'>Médecins</a>\n" +
+			"  <a href='hopitaux'>Hôpitaux</a>\n" +
+			"  <a href='contrats'>Contrats</a>\n" +
+			"  <a href='planning'>Planning</a>\n" +
 			"</nav>\n" +
+			"<hr>\n" +
 			'<ng-outlet></ng-outlet>\n',
 		$routeConfig:[
 		{path:'medecins', name:'Médecins', component:'medecins', useAsDefault:true},
@@ -21,76 +22,14 @@
 		{path:'contrats', name:'Contrats', component:'contrats'},
 		{path:'planning', name:'Planning', component:'planning'}
 		]
-	});		
+		}).component("hopitaux", {
+			templateUrl:"hopitaux.html"
+		}).component("medecins", {
+			templateUrl:"medecins.html"
+		}).component("contrats", {
+			templateUrl:"contrats.html"
+		}).component("planning", {
+			templateUrl:"planning.html"
+	});	
 	// fin
-			
-	/*
-	app.controller("menuController", ["$scope", function($scope){
-		$scope.medecins = function(){
-			loadPartial("medecins");
-		}
-		$scope.hopitaux = function(){
-			loadPartial("hopitaux");
-		}
-		$scope.contrats = function(){
-			loadPartial("contrats");
-		}
-		$scope.planning = function(){
-			loadPartial("planning");
-		}
-	}]);
-	
-	app.controller("partialController", ["$scope", function($scope){
-		$scope.test = function(){
-			console.log("test");
-		}
-	}]);
-		
-	
-	function loadPartial(page) {
-        switch (page) {
-            case 'medecins':
-				console.log("medecins");
-                $.ajax({
-                    url: 'medecins.html',
-                    type: 'GET',
-                    dataType: 'html',
-                    success: function (result) {
-                        $('#partial').html(result);
-                    }
-                });
-                break;
-            case 'hopitaux':
-                $.ajax({
-                    url: 'hopitaux.html',
-                    type: 'GET',
-                    dataType: 'html',
-                    success: function (result) {
-                        $('#partial').html(result);
-                    }
-                });
-                break;
-            case 'contrats':
-                $.ajax({
-                    url: 'contrats.html',
-                    type: 'GET',
-                    dataType: 'html',
-                    success: function (result) {
-                        $('#partial').html(result);
-                    }
-                });
-                break;
-            case 'planning':
-                $.ajax({
-                    url: 'planning.html',
-                    type: 'GET',
-                    dataType: 'html',
-                    success: function (result) {
-                        $('#partial').html(result);
-                    }
-                });
-                break;
-            
-        }
-    }*/
 })();
